de70c1c05f168cf682fbd4e0cdf21407442a579c
Douglas Machado <douglasgmachado98@gmail.com>
2025-05-20 15:22:05 -0300

    .

--- server/database/migrations/2025_04_22_231611_create_empresas_table.php
+++ server/database/migrations/2025_04_22_231611_create_empresas_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('empresas', function (Blueprint $table) {
             $table->id();
             $table->foreignId('usuario_id')->constrained('usuarios')->onDelete('cascade');
-            $table->string('cnpj')->unique()->nullable();
+            $table->string('cnpj')->nullable();
             $table->timestamps();
         });
     }
